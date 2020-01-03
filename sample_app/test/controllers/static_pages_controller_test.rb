dab8c0bc468a7ce9f7ee8d12c1ae6c9276ab9274
Munetaka <sakuraapp1988@gmail.com>
2020-01-04 00:14:55 +0900

    - About, Contact等のリンク先設定 - Integration Testの追加 - application_helperのテストを追加

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -7,35 +7,30 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get root" do
-    get root_url
+    get root_path
     assert_response :success
     # header要素があり、かつdiv -> a要素内のテキストが一致するか
     assert_select "header div a", "sample app"
     # footer要素があるか
     assert_select "footer"
-  end
-
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
   # Exercise : Contactページの作成
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
