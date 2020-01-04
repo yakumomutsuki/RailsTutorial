dd47043976bdf141c247b3b7f874a2efcc3c3c34
Munetaka <sakuraapp1988@gmail.com>
2020-01-04 16:42:53 +0900

    - User.emai => 一意制約を追加

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :name,  presence: true, length: { maximum: 50 }
+
+  # emailのバリデーション
+  # case_sensitiveは大文字小文字の違いを確認する制約であり、大文字小文字は不問としている
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX }
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
