4687182670ded84afd644d6ced132352f03da6a0
Munetaka <sakuraapp1988@gmail.com>
2020-01-13 15:48:49 +0900

    Add Some Tests

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -19,8 +19,8 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
 
   # 渡された文字列のハッシュ値を返す
-  def User.digest
-    cost = ActiveModel::SecurePassword.min_cost? BCrypt::Engine::MIN_COST :BCrypt::Engine.cost
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 end
