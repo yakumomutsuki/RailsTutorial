e89380575370780c615d9e8f29098c77e53cecad
Munetaka <sakuraapp1988@gmail.com>
2020-01-19 12:58:27 +0900

    Add remember cookie check box, some test added

--- sample_app/test/integration/users_login_test.rb
+++ sample_app/test/integration/users_login_test.rb
@@ -68,4 +68,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?('')
   end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    # クッキーを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    # クッキーを削除してログイン
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
 end
